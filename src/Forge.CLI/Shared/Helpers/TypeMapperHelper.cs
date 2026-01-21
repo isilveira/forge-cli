abe4e4b0a63d1545b6603cf6b0a193f9975abec0
Ítalo Silveira <italobrian@gmail.com>
2026-01-20 22:51:18 -0300

    Template Infrastructure.Mapping completed

--- src/Forge.CLI/Shared/Helpers/TypeMapperHelper.cs
+++ src/Forge.CLI/Shared/Helpers/TypeMapperHelper.cs
@@ -10,5 +10,19 @@
 			"datetime" => "DateTime",
 			_ => type
 		};
+		public static string DbMap(string type) => type switch
+		{
+			null => "NVARCHAR",
+			"" => "NVARCHAR",
+			"string" => "NVARCHAR",
+			"Guid" => "UNIQUEIDENTIFIER",
+			"datetime" => "DATETIME",
+			"small" => "TYNEINT",
+			"int" => "INT",
+			"long" => "BIGINT",
+			"decimal" => "DECIMAL",
+			"bool" => "BIT",
+			_ => type
+		};
 	}
 }
