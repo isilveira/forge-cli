1f59d014bb3a760ec41150b955e74cb93cefe483
Ítalo Silveira <italobrian@gmail.com>
2026-02-07 02:37:19 -0300

    default config sealed classes and virtual collections

--- src/Forge.CLI/Models/ForgeProject.cs
+++ src/Forge.CLI/Models/ForgeProject.cs
@@ -5,6 +5,8 @@
 		public string SchemaVersion { get; set; } = "1.0";
 		public string Name { get; set; } = default!;
 		public string DefaultIdType { get; set; } = "Guid";
+		public bool UseSealedClasses { get; set; } = true;
+		public bool UseVirtualCollections { get; set; } = false;
 		public string Tab { get; set; } = "    ";
 		public Dictionary<string, ForgeContext> Contexts { get; init; } = new();
 		public List<string> ScaffoldExceptions { get; init; } = new();
